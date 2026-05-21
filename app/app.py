c778b4d7d737eb63783f0c82a83d6a5deae57b64
Kamal Ahmed <kamalahmeddlg@gmail.com>
2026-05-21 13:14:42 +0530

    Final Render fix

--- app/app.py
+++ app/app.py
@@ -278,4 +278,8 @@ def add_header(response):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
